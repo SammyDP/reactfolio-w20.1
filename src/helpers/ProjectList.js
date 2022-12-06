fb2e69ad686b159e13659b701a8e9cdd83f272f1
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-12-06 10:34:09 -0700

    updated projects, still need to do contact & icons

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,39 @@
+import Proj1 from "../assets/proj1.png";
+import Proj2 from "../assets/proj2.png";
+import Proj3 from "../assets/proj3.png";
+import Proj4 from "../assets/proj4.png";
+import Proj5 from "../assets/proj5.png";
+import Proj6 from "../assets/proj6.png";
+
+export const ProjectList = [
+  {
+    name: "Weather Dashboard",
+    image: Proj1,
+    skills: "JavaScript,HTML,CSS,APIs",
+  },
+  {
+    name: "Music App",
+    image: Proj2,
+    skills: "HTML,CSS,JavaScript,Bulma",
+  },
+  {
+    name: "Password Generator",
+    image: Proj3,
+    skills: "HTML,CSS,JavaScript",
+  },
+  {
+    name: "Team Profile Generator",
+    image: Proj4,
+    skills: "JavaScript,Node.js,MySQL,GraphQL",
+  },
+  {
+    name: "Quiz Game",
+    image: Proj5,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Fantasy Fooball App",
+    image: Proj6,
+    skills: "Handlebars,JavaScript,HTML,CSS",
+  },
+];
