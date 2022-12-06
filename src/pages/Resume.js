04200afc9d25a9661d8f94cc6ce6a453c0785d2d
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-12-06 09:14:03 -0700

    Update Resume.js
    need to do projects and conctact

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,70 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 function Resume() {
-  return <div>Resume</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2010 - 2014"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon=""
+        >
+          <h3 className="vertical-timeline-element-title">
+            My Random High School, Random Place, Random State
+          </h3>
+          <p> High School Diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2014 - 2018"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon=""
+        >
+          <h3 className="vertical-timeline-element-title">Uinta Brewing</h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            Covered a territory of 3 states
+          </h4>
+
+          <p>Regional Brand Manager</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2020 - Present"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon=""
+        >
+          <h3 className="vertical-timeline-element-title">
+            Craft Bartender - Le Meridian/54Thirty
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">Denver, CO</h4>
+          <p>Have been Lead Bartender for the last 2 years.</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2022 - present"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon=""
+        >
+          <h3 className="vertical-timeline-element-title">
+            Full Stack Developer
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">Denver, CO</h4>
+          <p>
+            Recently completed a full stack bootcamp, and looking for
+            opportunities!
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Resume;
